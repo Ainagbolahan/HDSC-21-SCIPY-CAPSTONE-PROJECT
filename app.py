b396c1f6afa06e4ff8e1b59dee43255bc03f6883
Klonewalker <44479277+Klonewalker@users.noreply.github.com>
2022-02-20 10:16:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def prediction(Name, Platform, Year, Genre, Publisher):
     
     
     # Making predictions 
-    prediction = classifier.predict(dftype=float, [ 
-        Name, Platform, Year, Genre, Publisher])
+    prediction = classifier.predict([ 
+        Name, Platform, Year, Genre, Publisher], dtype="")
      
     output = round(prediction, 2)
 
