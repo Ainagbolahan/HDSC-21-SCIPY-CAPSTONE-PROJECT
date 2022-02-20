682f47aa3ae63880397c50d1c49f2cd9c22e0d39
Klonewalker <44479277+Klonewalker@users.noreply.github.com>
2022-02-20 10:39:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def prediction(Name, Platform, Year, Genre, Publisher):
      
     output = round(prediction, 2)
 
-    return 'Sales should be $ {output}'.format(output)
+    return 'Sales should be $ {}'.format(output)
 
       
   
@@ -82,7 +82,7 @@ def main():
     # when 'Predict' is clicked, make the prediction and store it 
     if st.button("Predict"):
         result = prediction
-        st.success('Sales should be {result}'.format(result))
+        st.success('Sales should be {}'.result)
         
      
 if __name__=='__main__': 
