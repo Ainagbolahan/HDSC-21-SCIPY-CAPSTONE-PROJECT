1b6212e768bc820e14c4e9d84e2aade40bbd850e
Klonewalker <44479277+Klonewalker@users.noreply.github.com>
2022-02-21 08:11:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,29 +22,29 @@ def prediction(Name, Platform, Year, Genre, Publisher):
  
     # Pre-processing user input    
     if Name != "":
-        Name = encoder.fit_transform([[Name]]).reshape(-1, 1)
+        Name = encoder.fit_transform(Name).reshape(-1, 1)
         
     else:
         Name = 0
      
     if Platform != "":
-        Platform = encoder.fit_transform([[Platform]]).reshape(-1, 1)
+        Platform = encoder.fit_transform(Platform).reshape(-1, 1)
         
     else:
         Platform = 0
         
     if Year != "":
-        Year = encoder.fit_transform([[Year]])
+        Year = encoder.fit_transform(Year)
     else:
         Year = 0
         
     if Genre != "":
-        Genre = encoder.fit_transform([[Genre]]).reshape(-1, 1)
+        Genre = encoder.fit_transform(Genre).reshape(-1, 1)
     else:
         Genre = 0
         
     if Publisher != "":
-        Publisher = encoder.fit_transform([[Publisher]]).reshape(-1, 1)
+        Publisher = encoder.fit_transform(Publisher).reshape(-1, 1)
     else:
         Publisher = 0
     
